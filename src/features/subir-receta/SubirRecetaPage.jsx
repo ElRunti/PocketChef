9b204fa06489c103bf8dc1421804ec6aa2ffa0cd
ElRunti <ruben060805@outlook.com>
2026-08-13 20:03:33 -0600

    "feat: crea estructura inicial para subir recetas"

---
+++ src/features/subir-receta/SubirRecetaPage.jsx
@@ -0,0 +1,11 @@
+import RecipeForm from "../../components/RecipeForm";
+
+function SubirRecetaPage() {
+    return(
+        <main>
+            <RecipeForm />
+        </main>
+    );
+}
+
+export default SubirRecetaPage;
---
+++ src/features/subir-receta/components/RecipeForm.jsx
@@ -0,0 +1,9 @@
+function RecipeForm() {
+  return (
+    <div>
+      <h2>Subir una nueva receta</h2>
+    </div>
+  );
+}
+
+export default RecipeForm;
